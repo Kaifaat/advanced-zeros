4ea737fce5a1629dbc1a12d0d61497a88f5653c4
Kaifaat <artem.meka@gmail.com>
2019-03-24 23:28:04 +0300

    feat: add zeros task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    let n = 0;
+    let i = 2;
+    let k = 0;
+    let array = new Array();
+
+    const baseNum = base;
+    while (i <= baseNum) {
+        if (base % i == 0) {
+            q = i;
+            n++;
+            base = base / i;
+        } else {
+            if (n != 0) {
+                array[k] = CountZeros(number, q, n);
+                k++;
+            }
+            i++;
+            n = 0;
+        }
+    }
+    return Math.min(...array);
+
+    function CountZeros(number, q, n) {
+        let sum = 0;
+        for (let i = 1; Math.pow(q, i) < number; i++) {
+            sum = sum + Math.floor(number / Math.pow(q, i));
+        }
+        return Math.floor(sum / n);
+    }
 }
+
